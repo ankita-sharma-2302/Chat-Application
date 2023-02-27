9372f67100803aadeb37c888979a456dffabf645
ankita-sharma-2302 <97795452+ankita-sharma-2302@users.noreply.github.com>
2023-02-27 11:38:18 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Styles/App.css';
+import './Styles/chat.css';
+import './Styles/style.css';
+import { Home } from './Components/Home';
+
+function App() {
+  return (
+    <div className="app">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
